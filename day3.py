06f3ec398f4a569540a24ad2c58c10166268a20b
Homura <2932785510@qq.com>
2026-09-15 21:49:29 +0800

    add day3 challenge

---
+++ day3.py
@@ -0,0 +1,13 @@
+name="Hommura"
+age=18
+city="见陇原"
+print(f"我叫{name},今年{age}岁,来自{city}.")
+s="Hello world"
+print(len(s))
+print(s.upper())
+print(s.lower())
+print(s.strip())
+print("    hello    " .strip())
+words="apple banana cherry".split()
+print(words)
+print(len(words))
