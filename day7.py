73c1e57080770cea8731afe02fcee42c05b19904
Homura <2932785510@qq.com>
2026-09-17 12:45:56 +0800

    add day7 challenge

---
+++ day7.py
@@ -0,0 +1,13 @@
+sum=0
+for i in range(1,101):
+    sum+=i
+print(sum)
+for i in range(1,11):
+    print(f"{i}的平方为{i**2}")
+n=10
+while n >0:
+    print(n)
+    n-=1
+for m in range(1,10):
+    for n in range(1,m+1):
+        print(f"{m}x{n}={m*n}")
